959683d735c76add564ce9a0bf8cbd6b7edf45ce
aryaziai <arya721@gmail.com>
2020-07-14 17:30:30 -0700

    Set up App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Testimonials from "./Testimonials.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Testimonials</h1>
+        <Testimonials />
+      </header>
+    </div>
+  );
+}
+
+export default App;
